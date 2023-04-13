bd7d330a9b43bcb78aa3128b733953246030cda8
Emanuel <emanuel.reyes@obf.ateneo.edu>
2023-04-14 02:29:55 +0800

    Added PCF

--- Exercise-3/demo8.fs
+++ Exercise-3/demo8.fs
@@ -26,32 +26,35 @@ out vec4 fragmentColor;
 in vec4 shaderLightSpacePosition;
 uniform sampler2D shadowMap;
 
-bool inShadow()
+float inShadow(vec4 shaderLightSpacePosition)
 {
     // perform perspective division and rescale to the [0, 1] range to get the coordinates into the depth texture
     vec3 position = shaderLightSpacePosition.xyz / shaderLightSpacePosition.w;
     position = position * 0.5f + 0.5f;
-
-    // if the position is outside the light-space frustum, do NOT put the
-    // fragment in shadow, to prevent the scene from becoming dark "by default"
-    // (note that if you have a spot light, you might want to do the opposite --
-    // that is, everything outside the spot light's cone SHOULD be dark by default)
-    if (position.x < 0.0f || position.x > 1.0f
-        || position.y < 0.0f || position.y > 1.0f
-        || position.z < 0.0f || position.z > 1.0f)
-    {
-        return false;
-    }
 
     // access the shadow map at this position
     float shadowMapZ = texture(shadowMap, position.xy).r;
 
     // add a bias to prevent shadow acne
     float bias = 0.0005f;
-    shadowMapZ += bias;
+
+    float shadow = 0.0;
+    vec2 texels = 1.0 / textureSize(shadowMap, 0);
+
+    for(int x = -1; x <= 1; ++x) {
+        for(int y = -1; y <= 1; ++y) {
+        float pcf = texture(shadowMap, position.xy + vec2(x, y) * texels).r; 
+        shadow += position.z - bias > pcf ? 1.0 : 0.0;        
+        }    
+    }
+    shadow /= 9.0;
+
+    if(position.z > 1.0) {
+        shadow = 0.0;
+    }
 
     // if the depth stored in the texture is less than the current fragment's depth, we are in shadow
-    return shadowMapZ < position.z;
+    return shadow;
 }
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -91,12 +94,12 @@ void main()
 
     ///////////////////////////////////////////////////////////////////////////
     // zero-out the diffuse and specular components if the fragment is in shadow
-    if (inShadow() && isShadowsOn == 1)
+    /*if (inShadow() && isShadowsOn == 1)
     {
         lightDiffuse = lightSpecular = vec3(0.0f, 0.0f, 0.0f);
-    }
+    }*/
     ///////////////////////////////////////////////////////////////////////////
-
+    float shadow = inShadow(shaderLightSpacePosition);
     // compute final fragment color
-    fragmentColor = vec4((lightAmbient + lightDiffuse + lightSpecular), 1.0f) * texture(diffuseMap, shaderTexCoord);
+    fragmentColor = vec4((lightAmbient + (1.0 - shadow) * (lightDiffuse + lightSpecular)), 1.0f) * texture(diffuseMap, shaderTexCoord);
 }
